9becb5ae73e5eb57b1c225993e79d6d03cf4afb0
MargaritaLtk <123299004+MargaritaLtk@users.noreply.github.com>
2024-01-14 00:19:46 +0300

    Update note.js

--- note.js
+++ note.js
@@ -5,3 +5,20 @@ function highAndLow(numbers){
   numbers = numbers.split(' ').map(Number);
   return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
 }
+
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+        default:
+            return 0;
+    }
+}
+
+

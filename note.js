adb6a89c5fa50f4f4bff73cc019dd79a53f09d10
MargaritaLtk <123299004+MargaritaLtk@users.noreply.github.com>
2024-01-19 23:29:07 +0300

    Update note.js

--- note.js
+++ note.js
@@ -1,24 +1,19 @@
-//check reduce(), repeat();
-
-//
-function highAndLow(numbers){
-  numbers = numbers.split(' ').map(Number);
-  return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+function highAndLow(numbers) {
+  numbers = numbers.split(" ").map(Number);
+  return Math.max.apply(0, numbers) + " " + Math.min.apply(0, numbers);
 }
 
 function basicOp(operation, value1, value2) {
-    switch (operation) {
-        case '+':
-            return value1 + value2;
-        case '-':
-            return value1 - value2;
-        case '*':
-            return value1 * value2;
-        case '/':
-            return value1 / value2;
-        default:
-            return 0;
-    }
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+    case "-":
+      return value1 - value2;
+    case "*":
+      return value1 * value2;
+    case "/":
+      return value1 / value2;
+    default:
+      return 0;
+  }
 }
-
-
